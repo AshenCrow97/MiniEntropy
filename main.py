451cdd7aa441d54c21b9b59aa7229fe0ccd3e81b
Aleš Calábek <xcalabek@fi.muni.cz>
2025-01-28 17:16:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+# Resizer model
+# transform method
+
+
+# Color model
+# fit, transform methods
+
+
+# DecisionTree model
+# returns a list of cells - four coordinates and color
+
+
+# Draw model
+# draws the cells
+# you can specify outline and other visual stuff
