d1da06a2675fc8199560daf537fe776c26039573
AshenCrow97 <alescalabek97@gmail.com>
2025-01-28 17:38:38 +0100

    Initializing Resizer class

---
+++ resizer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+
+
+class Resizer:
+    """
+    """
+
+    def fit(self, path):
+        pass
+
+
+    def transform(self):
+        pass
+
+
+    def fit_transform(self):
+        pass
