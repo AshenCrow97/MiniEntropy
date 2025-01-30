debe1f08c5ea95f2d151bc256c3683281c69b188
AshenCrow97 <alescalabek97@gmail.com>
2025-01-30 12:30:10 +0100

    Add skeleton code for PaletteMaker class

--- palette_maker.py
+++ palette_maker.py
@@ -1,27 +1,47 @@
 #!/usr/bin/env python3
 
+import numpy as np
+from PIL import Image
+from sklearn.cluster import KMeans
+from typing import Self
+
 
 class PaletteMaker:
+    """
+    """
 
     def __init__(self):
         pass
+        self.k_colors = 4
+        self.model = KMeans(n_clusters=self.k_colors, n_init=1, random_state=0)
 
 
-    def fit():
-        pass
+    def fit(self, pixels: np.ndarray) -> Self:
+        """
+        """
 
+        np.random.seed(0)
+        rng = np.random.default_rng()
 
-    def transform():
-        pass
+        sample_size = np.sqrt(pixels.shape[0]).astype(int)
+        sample = rng.choice(pixels, size=sample_size)
 
+        self.model.fit(sample)
 
-    def fit_transform():
-        pass
-
+        return self
     
 
-# def foo():
-#     image = Image.open(filename)
-#     image, width, height = resize(image)
-#     pixels = np.array( image.getdata() )
-#
+    def transform(self, pixels: np.ndarray) -> np.ndarray:
+        """
+        """
+
+        return self.model.predict(pixels)
+
+
+    def fit_transform(self, pixels: np.ndarray) -> np.ndarray:
+        """
+        """
+
+        return self.fit(pixels).transform(pixels)
+
+    
