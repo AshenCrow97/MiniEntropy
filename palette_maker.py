cec8d26e80d88d1a17e0b4051835e69f983f86d0
AshenCrow97 <alescalabek97@gmail.com>
2025-01-30 11:40:54 +0100

    Initialize PaletteMaker class

---
+++ palette_maker.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+class PaletteMaker:
+
+    def __init__(self):
+        pass
+
+
+    def fit():
+        pass
+
+
+    def transform():
+        pass
+
+
+    def fit_transform():
+        pass
+
+    
+
+# def foo():
+#     image = Image.open(filename)
+#     image, width, height = resize(image)
+#     pixels = np.array( image.getdata() )
+#
