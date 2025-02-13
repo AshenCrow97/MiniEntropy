86c2506d3f567c660df897a99290117a1e54fd8b
AshenCrow97 <alescalabek97@gmail.com>
2025-02-13 10:10:57 +0100

    Add template for tranformer model

---
+++ skeleton_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+from typing import Self, Any
+
+
+class SkeletonModel:
+    """ 
+    """
+
+    def __init__(self):
+        """
+        """
+
+        pass
+
+
+    def fit(self, X: Any, show: bool = False) -> Self:
+        """
+        """
+
+        return self
+
+
+    def transform(self, X: Any, show: bool = False) -> Any:
+        """
+        """
+
+        return X
+
+
+    def fit_transform(self, X: Any, show: bool = False) -> Any:
+        """
+        """
+        
+        return self.fit(X).transform(X, show)
+
+
+if __name__ == '__main__':
+
+    pass
+
