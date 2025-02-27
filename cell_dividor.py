3f994f10f26680772d27df0b2d67c90f33e7aa4e
AshenCrow97 <acalabek97@gmail.com>
2025-02-27 18:21:32 +0100

    Rfac to pass data as dict

--- cell_dividor.py
+++ cell_dividor.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 
-class CellDividor:
-    """
+
+from typing import Any, Dict, Self
+
+
+class CellDivider:
+    """ 
     """
 
     def __init__(self):
+        """
+        """
+
         pass
 
 
-    def transform(self):
-        pass
-        # return image, cells
+    def fit(self, X: Dict[str, Any], show: bool = False) -> Self:
+        """
+        """
+
+        return self
+
+
+    def transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """
+        """
+
+        return X
+
+
+    def fit_transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """
+        """
+        
+        return self.fit(X).transform(X, show)
+
+
+if __name__ == '__main__':
+
+    pass
+
