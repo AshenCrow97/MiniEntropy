21d8461d606b44aa3bcf81c08258d9b7307cb3b2
AshenCrow97 <acalabek97@gmail.com>
2025-03-01 23:17:27 +0100

    Init class to draw cells

---
+++ draw.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+from typing import Any, Dict, List, Self
+from PIL import Image, ImageDraw
+from cell import Cell
+
+
+class CellDrawer:
+    """ 
+    """
+
+    def __init__(self):
+        """
+        """
+        pass
+    
+
+    def fit(self, X: Dict[str, Any], show: bool = False) -> Self:
+        """
+        """
+
+        return self
+
+
+    def transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """
+        """
+
+        image = Image.new('RGBA', (X["width"], X["height"]), (0, 0, 0, 0))
+        draw = ImageDraw.Draw(image)
+
+        for cell in X["cells"]:
+            draw.rectangle(
+                [cell.x1, cell.y1, cell.x2, cell.y2],
+                fill=(cell.r, cell.g, cell.b, cell.a)
+            )
+
+        if show:
+            image.show()
+
+        X["cellular_image"] = image
+
+        return X
+
+
+    def fit_transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """
+        """
+        
+        return self.fit(X, show).transform(X, show)
+
+
+if __name__ == '__main__':
+
+    pass
