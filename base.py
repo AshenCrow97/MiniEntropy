82aaee3c13aab14e92047e3827bd15e72e33085e
AshenCrow97 <acalabek97@gmail.com>
2025-03-03 10:37:42 +0100

    Rename SkeletonModel to BaseModel, add docstrings

---
+++ base.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+
+from typing import Any, Dict, Self
+
+
+class BaseModel:
+    """Base class for all models used in the pipeline."""
+
+
+    def __init__(self):
+        """Initialize the model."""
+
+        pass
+
+
+    def fit(self, X: Dict[str, Any], show: bool = False) -> Self:
+        """Process input data and fit the model.
+
+        Parameters
+        ----------
+        X : Dict[str, Any]
+            A dictionary of input data.
+
+        show : bool, default=False
+            Whether to display results.
+        
+        Returns
+        -------
+        self : BaseModel
+            Fitted model.
+        """
+
+        return self
+
+
+    def transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """Process input data and transform it.
+
+        Parameters
+        ----------
+        X : Dict[str, Any]
+            A dictionary of input data.
+        
+        show : bool, default=False
+            Whether to display results.
+
+        Returns
+        -------
+        X : Dict[str, Any]
+            The input dictionary with added transformed data.
+        """
+
+        return X
+
+
+    def fit_transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
+        """Process data, fit the model, and transform the data.
+
+        Parameters
+        ----------
+        X : Dict[str, Any]
+            A dictionary of input data.
+
+        show : bool, default=False
+            Whether to display results.
+
+        Returns
+        -------
+        X : Dict[str, Any]
+            The input dictionary with added transformed data.
+        """
+        
+        return self.fit(X, show).transform(X, show)
+
