19ad49431a51e7ec545c72663a69e19ef86e7c47
AshenCrow97 <acalabek97@gmail.com>
2025-03-03 10:43:19 +0100

    Add attributes to constructor, inheritance from BaseModel

--- draw.py
+++ draw.py
@@ -1,26 +1,22 @@
 #!/usr/bin/env python3
 
-from typing import Any, Dict, List, Self
+from typing import Any, Dict, Self
 from PIL import Image, ImageDraw
 from cell import Cell
+from base import BaseModel
 
 
-class CellDrawer:
+class CellDrawer(BaseModel):
     """ 
     """
 
-    def __init__(self):
+    def __init__(self, outline : str = "black", width : int = 1):
         """
         """
-        pass
+
+        self.outline = outline
+        self.width = width
     
-
-    def fit(self, X: Dict[str, Any], show: bool = False) -> Self:
-        """
-        """
-
-        return self
-
 
     def transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
         """
@@ -32,7 +28,10 @@ class CellDrawer:
         for cell in X["cells"]:
             draw.rectangle(
                 [cell.x1, cell.y1, cell.x2, cell.y2],
-                fill=(cell.r, cell.g, cell.b, cell.a)
+                fill=(cell.r, cell.g, cell.b, cell.a),
+                outline=self.outline,
+                width=self.width,
+                
             )
 
         if show:
@@ -42,14 +41,3 @@ class CellDrawer:
 
         return X
 
-
-    def fit_transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
-        """
-        """
-        
-        return self.fit(X, show).transform(X, show)
-
-
-if __name__ == '__main__':
-
-    pass
