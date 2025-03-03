d74ded7e79ac34589498736071ceae2ce8307045
AshenCrow97 <acalabek97@gmail.com>
2025-03-03 11:08:52 +0100

    Add docstrings

--- draw.py
+++ draw.py
@@ -7,19 +7,38 @@ from base import BaseModel
 
 
 class CellDrawer(BaseModel):
-    """ 
+    """Model that draws colored cells on an image.
+
+    Attributes
+    ----------
+    outline : str, default="black"
+        The color of the cell outline.
+    
+    width : int, default=1
+        The width of the cell outline.
     """
 
     def __init__(self, outline : str = "black", width : int = 1):
-        """
-        """
 
         self.outline = outline
         self.width = width
     
 
     def transform(self, X: Dict[str, Any], show: bool = False) -> Dict[str, Any]:
-        """
+        """Create new image with colored cells drawn on it.
+
+        Parameters
+        ----------
+        X : Dict[str, Any]
+            A dictionary of input data.
+
+        show : bool, default=False
+            Whether to display the image.
+
+        Returns
+        -------
+        X : Dict[str, Any]
+            The input dictionary with added image of colored cells.
         """
 
         image = Image.new(mode='RGBA', size=(X["width"], X["height"]))
